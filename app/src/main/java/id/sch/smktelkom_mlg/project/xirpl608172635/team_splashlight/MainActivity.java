83080942de6c255688ba82d5d3c87a73df1e6bdb
daniamri <daniamri93@gmail.com>
2016-11-17 06:37:38 +0700

    Error Message And Off Button

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl608172635/team_splashlight/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl608172635/team_splashlight/MainActivity.java
@@ -1,13 +1,58 @@
 package id.sch.smktelkom_mlg.project.xirpl608172635.team_splashlight;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+    Camera camera;
+    Camera.Parameters parameters;
+    boolean isflash = false;
+    boolean isOn = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            camera = Camera.open();
+            parameters = camera.getParameters();
+            isflash = true;
+
+        }
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isflash) {
+
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Error....");
+                    builder.setMessage("Flashlight is not Available on this device...");
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+            }
+        });
     }
+
 }
